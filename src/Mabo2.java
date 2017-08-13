074845edfaccd41dbd4e5a84191e30db594e693e
Motovilov Roman <bombey131@gmail.com>
2017-08-13 16:19:20 +0200

    change operator name

--- src/Mabo2.java
+++ src/Mabo2.java
@@ -8,7 +8,7 @@ public class Mabo2 {
         Wypalarka wypalarka = new Wypalarka("Roman", "Wypalarka");
         System.out.println(wypalarka.nameWorkerAndMachine());
 
-        Lazer lazer = new Lazer("Dick");
+        Lazer lazer = new Lazer("John");
         lazer.getDetails();
         lazer.getWorker();
     }
